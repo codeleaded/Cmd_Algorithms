6799f7ed0eb67ed636c52ce621f98c5f8ec12d0f
codeleaded <codeleaded@gmail.com>
2025-11-06 18:17:25 +0100

    update

---
+++ src/Main.c
@@ -0,0 +1,16 @@
+#include "/home/codeleaded/System/Static/Container/Algorithms.h"
+
+int main(){
+    /*
+    Search_Linear
+    Search_Binary
+    Search_Interpolation
+    Sort_Bubble
+    
+    Sort_Selection
+    Sort_Insertion
+    Sort_Merge
+    Sort_Quick
+    */
+    return 0;
+}
